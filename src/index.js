1c1b4afe4864fc3ec1be09b9527ec18ae737f2e7
Adam <snx123123@gmail.com>
2026-05-21 20:08:43 +0200

    style: utilisation du lien direct imgur pour le gif

--- src/index.js
+++ src/index.js
@@ -105,7 +105,7 @@ client.on('guildMemberAdd', async (member) => {
         .setDescription(`Bonjour ${member} ! Nous sommes ravis de t'accueillir parmi nous.\n\n💼 **Nancy ASSISTANCE** et l'ensemble de la communauté te souhaitent une excellente intégration. Prépare ton plus beau rôleplay, l'aventure commence ici ! ✨`)
         .setColor("#5865F2")
         .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
-        .setImage("https://cdn.discordapp.com/attachments/1505943854250201118/1507075598021951508/NANCY_RP_5.gif?ex=6a109506&is=6a0f4386&hm=6939277976e0258d9492df40ca9a18992b2fbf60a9c1e39c1317a5e270655375&")
+        .setImage("https://i.imgur.com/B9OOniM.gif")
         .setFooter({ text: `Nancy ASSISTANCE • Nous sommes désormais ${member.guild.memberCount} citoyens !` })
         .setTimestamp();
 
@@ -122,7 +122,7 @@ client.on('guildMemberRemove', async (member) => {
         .setDescription(`**${member.user.tag}** a pris ses bagages et vient de quitter le territoire de Nancy RP.\n\nToute l'équipe de **Nancy ASSISTANCE** lui souhaite une bonne continuation dans ses futurs projets. Sa carte de citoyen a été archivée. 🗃️`)
         .setColor("#FF3333")
         .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
-        .setImage("https://cdn.discordapp.com/attachments/1505943854250201118/1507075598021951508/NANCY_RP_5.gif?ex=6a109506&is=6a0f4386&hm=6939277976e0258d9492df40ca9a18992b2fbf60a9c1e39c1317a5e270655375&")
+        .setImage("https://i.imgur.com/B9OOniM.gif")
         .setFooter({ text: `Nancy ASSISTANCE • Il reste ${member.guild.memberCount} citoyens en ville.` })
         .setTimestamp();
 
